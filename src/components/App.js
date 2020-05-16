52c65000a580f52c2c020fb870911bfe4305cf73
Eliottnibley <e.nibley@gmail.com>
2020-05-16 11:14:30 -0600

    update

--- src/components/App.js
+++ src/components/App.js
@@ -34,10 +34,15 @@ class App extends Component {
 
   deletePost(id) {
     axios.delete(`https://practiceapi.devmountain.com/api/posts?id=${id}`)
+    .then(res => this.setState({posts: res.data}))
+    .catch(err => console.log('post did not delete'))
   }
 
-  createPost() {
-
+  createPost(text) {
+    axios.post(`https://practiceapi.devmountain.com/api/posts`, {
+      text: text
+    })
+    .then(res => this.setState({posts: res.data}))
   }
 
   render() {
@@ -49,9 +54,9 @@ class App extends Component {
 
         <section className="App__content">
 
-          <Compose />
+          <Compose createPostFn={this.createPost} />
           {posts.map(elem => {
-             return <Post text={elem.text} date={elem.date} updatePostFn={this.updatePost} id={elem.id} key={elem.id}/>
+             return <Post text={elem.text} date={elem.date} deletePostFn={this.deletePost} updatePostFn={this.updatePost} id={elem.id} key={elem.id}/>
             })
           }
           
